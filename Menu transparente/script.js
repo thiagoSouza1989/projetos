fddcbe5ee75b4292870cae20dbfab040692ceb27
Thiago Souza dos Santos <115598075+thiagoSouza1989@users.noreply.github.com>
2023-06-27 21:33:51 -0300

    troca da cor do texto

--- Menu transparente/script.js
+++ Menu transparente/script.js
@@ -1,4 +1,4 @@
 window.addEventListener("scroll", function(){
     var header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 200)
+    header.classList.toggle('rolagem', window.scrollY > 100)
 })
