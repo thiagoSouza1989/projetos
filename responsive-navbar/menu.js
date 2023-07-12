b3aa6a838b81acbc14bdc340f050cd4694862d1e
Thiago Souza dos Santos <115598075+thiagoSouza1989@users.noreply.github.com>
2023-07-11 21:05:04 -0300

    projeto finalizado

--- responsive-navbar/menu.js
+++ responsive-navbar/menu.js
@@ -4,4 +4,9 @@ const dropDownMenu = document.querySelector('.dropdown_menu')
 
 toggleBtn.onclick = function () {
     dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
 }
